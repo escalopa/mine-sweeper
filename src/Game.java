21d0ad6c6b6f9f852162cf6037d4701acb420653
escalopa <ahmad.helaly.dev@gmail.com>
2022-09-20 23:29:50 +0300

    add cmd version code

--- src/Game.java
+++ src/Game.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 public class Game {
 
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        final int dimension = 15;
-        final int unit = 50;
-        new WindowFrame().run(dimension, unit);
+        new Minesweeper(15,20).startGame();
+
+//        final int dimension = 15;
+//        final int unit = 50;
+//        new WindowFrame().run(dimension, unit);
     }
 }
