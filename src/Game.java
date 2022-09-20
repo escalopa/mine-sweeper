70b4bb127d2b678a03defefe34dafbdf66922e7d
escalopa <ahmad.helaly.dev@gmail.com>
2022-09-20 23:40:45 +0300

    remove cmd code

--- src/Game.java
+++ src/Game.java
@@ -7,10 +7,8 @@ import java.io.IOException;
 public class Game {
 
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        new Minesweeper(15,20).startGame();
-
-//        final int dimension = 15;
-//        final int unit = 50;
-//        new WindowFrame().run(dimension, unit);
+        final int dimension = 15;
+        final int unit = 50;
+        new WindowFrame().run(dimension, unit);
     }
 }
